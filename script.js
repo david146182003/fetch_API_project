4a6e2cdc84aa72c649fd0bb9c76532dfff1b188c
david <david146182003@gmail.com>
2025-06-06 03:25:11 -0400

    adding html input

--- script.js
+++ script.js
@@ -3,28 +3,36 @@
 const API_key ="live_p21Nx9H3hwYXJy56fUAVVM6OPrgNiEVUoEhP8arnWMRYurOgAoQn7y2TXc0pHKXI";
 const divEl = document.querySelector("div");
 const catSelectEl = document.getElementById("catSelect");
-const imgListEl = document.getElementById("imgList")
+const imgEl = document.getElementById("catImg")
 
 const fetchData = async()=>{
     const resopnse = await fetch("https://api.thecatapi.com/v1/breeds");
     const data = await resopnse.json();
     console.log(data)
+    
     data.forEach(breed =>{
-        const option = document.createElement("option");
-        option.textContent = breed.name;
-        catSelectEl.appendChild(option)
+        const createOption = document.createElement("option");
+        createOption.textContent = breed.name;
+        catSelectEl.appendChild(createOption)
+        
+        
+        // breed.addEvenLister('click', ()=>{
+            
+        // })
     })
 }
 fetchData()
 
-const getImage = async()=>{
+const getImage = async(breedId)=>{
     const resopnse = await fetch(`https://api.thecatapi.com/v1/images/search?limit=1&breed_ids=beng&api_key=${API_key}`);
     const img = await resopnse.json();
     console.log(img)
-    img.forEach(img =>{
-        const imgEl = document.createElement('img');
-        imgEl.setAttribute('src', img.url)
-    })
-    console.log(img)
+    imgEl.setAttribute("src", img[0].url)
+    
 }
-getImage()
+
+catSelectEl.addEventListener('change', (e)=>{
+    const selectBreedId = e.target.value;
+    console.log(e.target.value)
+    getImage(selectBreedId)
+})
